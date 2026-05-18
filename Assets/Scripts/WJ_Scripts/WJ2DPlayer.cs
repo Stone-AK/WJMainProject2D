ea5e932c6278d2bc9225c82590ffa6dffd2188b5
WooJae An <ays97123@gmail.com>
2026-05-18 10:40:48 +0900

    [리서스 업로드]

---
+++ Assets/Scripts/WJ_Scripts/WJ2DPlayer.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+public enum Player2DAnimeState
+{
+    None = 0,
+    Idle,
+    LeftWalk,
+    RightWalk
+}
+
+public class WJ2DPlayer : MonoBehaviour
+{
+    [Header("이동 설정")]
+    [SerializeField] private Rigidbody2D PlayerRigidBody;
+
+    [Header("점프 설정")]
+    [SerializeField] private float jumpForce = 8f;
+
+    [Header("바닥 체크")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("상태")]
+    [SerializeField] private float _moveInput;
+    [SerializeField] private bool _isGround;
+    [SerializeField] private WJ2DPlayerAnimation PlayerAnime;
+
+
+    [Header("애니메이션 컨트롤 스크립트")]
+    [SerializeField] private WJ2DPlayerAnimation PlayerAnimaController;
+
+    [Header("UI")]
+    [SerializeField] private PlayerStatUI PlayerStatUI;
+
+    private int _hp;
+    private int _curHP;
+    private float moveSpeed = 5f;
+
+    private void Update()
+    {
+        _moveInput = Input.GetAxisRaw("Horizontal");
+
+        _isGround = CheckCharactorIsGround();
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+        {
+            JumpCharactor();
+            PlayerAnimaController.ChangeMoveAnimation(Player2DAnimStat.JumpStart);
+        }
+        MoveCharactor();
+
+        if (_isGround)
+        {
+            PlayerAnimaController.ChangeMoveAnimation(Player2DAnimStat.JumpEnd);
+            if (_moveInput > 0)
+                PlayerAnimaController.ChangeMoveAnimation(Player2DAnimStat.RightWalk);
+            else if (_moveInput < 0)
+                PlayerAnimaController.ChangeMoveAnimation(Player2DAnimStat.LeftWalk);
+            else if (_moveInput == 0)
+                PlayerAnimaController.ChangeMoveAnimation(Player2DAnimStat.Idle);
+        }
+        if (!_isGround)
+            PlayerAnimaController.ChangeMoveAnimation(Player2DAnimStat.JumpLoop);
+    }
+
+    private void MoveCharactor()
+    {
+        PlayerRigidBody.linearVelocity = new Vector2(
+            _moveInput * moveSpeed,
+            PlayerRigidBody.linearVelocity.y
+        );
+    }
+
+    private void JumpCharactor()
+    {
+        PlayerRigidBody.linearVelocity = new Vector2(PlayerRigidBody.linearVelocity.x, jumpForce);
+    }
+
+    private bool CheckCharactorIsGround()
+    {
+        bool isGround;
+        isGround = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+        return isGround;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _curHP -= damage;
+        PlayerStatUI.ShowHP(_hp, _curHP);
+        if (0 >= _curHP)
+        {
+            DaniTechGameManager.Inst.GameOver();
+        }
+    }
+
+    public void SetPlayerData(int setHp, float setMoveSpeed)
+    {
+        _hp = setHp;
+        moveSpeed = setMoveSpeed;
+        _curHP = _hp;
+        PlayerStatUI.InitUI(_hp, _curHP);
+    }
+    public void PrintStat()
+    {
+        Debug.Log($"{this.gameObject.name}의 HP는 {_curHP}");
+        Debug.Log($"{this.gameObject.name}의 이동속도는 {moveSpeed}");
+    }
+}
---
+++ Assets/Scripts/WJ_Scripts/WJ2DPlayerAnimation.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public enum Player2DAnimStat
+{
+    None = 0,
+    Idle,
+    RightWalk,
+    LeftWalk,
+    JumpStart,
+    JumpLoop,
+    JumpEnd
+}
+
+public class WJ2DPlayerAnimation : MonoBehaviour
+{
+    [Header("애니메이터")]
+    [SerializeField] private Animator CharactorAnimator;
+
+    [Header("상태")]
+    [SerializeField] private Player2DAnimStat _CharactorAnimeState;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    public void ChangeMoveAnimation(Player2DAnimStat ChangeStat)
+    {
+        _CharactorAnimeState = ChangeStat;
+        switch (_CharactorAnimeState)
+        {
+            case Player2DAnimStat.Idle:
+                ResetAllAnimParameters();
+                break;
+            case Player2DAnimStat.RightWalk:
+                _spriteRenderer.flipX = false;
+                CharactorAnimator.SetBool("isMove", true);
+                break;
+            case Player2DAnimStat.LeftWalk:
+                _spriteRenderer.flipX = true;
+                CharactorAnimator.SetBool("isMove", true);
+                break;
+            case Player2DAnimStat.JumpStart:
+                CharactorAnimator.SetTrigger("Jump");
+                break;
+            case Player2DAnimStat.JumpLoop:
+                CharactorAnimator.SetBool("isAir", true);
+                break;
+            case Player2DAnimStat.JumpEnd:
+                CharactorAnimator.SetBool("isAir", false);
+                break;
+            default:
+                Debug.Log("지정된 애니메이션이 없습니다.");
+                break;
+        }
+    }
+
+    private void ResetAllAnimParameters()
+    {
+        // CharactorAnimator.SetBool("isAir", false);
+        CharactorAnimator.SetBool("isMove", false);
+    }
+}
